fb43068ce6bce38cf4212e3ca4b77289b1dd3360
StaticReflection <deng3616888303@gmail.com>
2026-02-11 22:33:52 +0800

    feat(network): implement DioClient and network configuration
    - Add NetworkConfig with baseUrl and timeout settings
    - Implement DioClient wrapper with BaseOptions initialization
    - Register Dio and DioClient as singletons in service locator

--- lib/core/di/core.dart
+++ lib/core/di/core.dart
@@ -3,6 +3,9 @@ part of 'injection_container.dart';
 Future<void> _initCoreDI() async {
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerSingleton<SharedPreferences>(sharedPreferences);
+  
+  sl.registerLazySingleton<Dio>(() => Dio());
+  sl.registerLazySingleton<DioClient>(() => DioClient(sl()));
 
   sl.registerLazySingleton<SettingsLocalDataSource>(
     () => SettingsLocalDataSourceImpl(sl()),
--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,17 +1,18 @@
-import 'package:flutter_template/data/sources/local/settings/settings_local_data_source.dart';
-import 'package:flutter_template/domain/usecases/core/get_locale_use_case.dart';
-import 'package:flutter_template/domain/usecases/core/get_theme_mode_use_case.dart';
-import 'package:flutter_template/domain/usecases/core/set_locale_use_case.dart';
-import 'package:flutter_template/domain/usecases/core/set_theme_mode_use_case.dart';
 import 'package:get_it/get_it.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
 
 import 'package:flutter_template/presentation/common/logic/locale/locale_cubit.dart';
 import 'package:flutter_template/data/repositories/settings_repository_impl.dart';
 import 'package:flutter_template/domain/repositores/settings_repository.dart';
 import 'package:flutter_template/presentation/common/logic/theme/theme_cubit.dart';
 import 'package:flutter_template/presentation/home/bloc/home_bloc.dart';
+import 'package:flutter_template/core/network/dio_client.dart';
+import 'package:flutter_template/data/sources/local/settings/settings_local_data_source.dart';
+import 'package:flutter_template/domain/usecases/core/get_locale_use_case.dart';
+import 'package:flutter_template/domain/usecases/core/get_theme_mode_use_case.dart';
+import 'package:flutter_template/domain/usecases/core/set_locale_use_case.dart';
+import 'package:flutter_template/domain/usecases/core/set_theme_mode_use_case.dart';
 
 part 'core.dart';
 
